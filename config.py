658a2b403ecc5dcfccc0c256507019981d623e50
Werdsaf <117178892+Werdsaf@users.noreply.github.com>
2025-03-01 14:01:26 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+# config.py
+BOT_TOKEN = "7872778132:AAGJF2Ueejc4VdasF2QTECEehKdJa5_rTQg"  # Замените на свой фактический токен
+WEB_APP_URL = "https://cepren14.github.io/bot.github.io/" # Замените на url веб приложения
