e6477684d6036d2010cdaaf57364f4dcda38a09e
CepreN14 <CepreN14@gmail.com>
2025-03-02 00:57:41 +0700

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-# config.py
-BOT_TOKEN = "7758370360:AAF4tg85f-J0WVzLIHm-eaea9_DkY0vLjCM"  # Замените на свой фактический токен
-WEB_APP_URL = "https://cepren14.github.io/bot.github.io/" # Замените на url веб приложения
-DATABASE_URL = "sqlite:///./app.db"
